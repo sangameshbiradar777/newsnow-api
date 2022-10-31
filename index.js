e9702232d5ab2ba944615d9d7939284310f0a393
Sangamesh <sangameshbiradar586141@gmail.com>
2022-10-31 10:48:02 +0530

    Newsnow api

---
+++ index.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import fetch from 'node-fetch';
+
+const app = express();
+
+const PORT = 5000;
+
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+app.get('/', (req, res) => {
+  res.send('Hello from news now Express server')
+})
+
+app.get('/getnews/', (req, res) => {
+  const URL = 'https://guarded-beach-97677.herokuapp.com/' + 'https://newsapi.org/v2/everything?q=sbi&searchIn=title&language=en&page=1&pageSize=10&apiKey=b720fe057ca34e46809d9e53a42ac6bd';
+  // res.send(URL)
+  fetch(URL, {headers: {
+    'origin': 'http://localhost:5000',
+    'X-Requested-With': 'XMLHttpRequest'
+  }}).then(data => {
+    // data.header("Access-Control-Allow-Origin", "http://localhost:5500");
+    console.log(data);
+    res.send(data);
+  }).then(data => {
+    console.log(data);
+    // res.send(data);
+  })
+})
+
