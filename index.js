53fb8c6a280a5ea0259011b269176d99f776c753
Sangamesh <sangameshbiradar586141@gmail.com>
2022-12-03 12:24:29 +0530

    Changed from heroku to render

--- index.js
+++ index.js
@@ -1,31 +1,34 @@
-import express from 'express';
-import fetch from 'node-fetch';
-import cors from 'cors';
+import express from "express";
+import fetch from "node-fetch";
+import cors from "cors";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello from news now Express server')
-})
+app.get("/", (req, res) => {
+  res.send("Hello from news now Express server");
+});
 
-app.get('/getnews/:url', (req, res) => {
-  const URL = 'https://guarded-beach-97677.herokuapp.com/' + req.params.url;
+app.get("/getnews/:url", (req, res) => {
+  const URL = "https://proxy-server-1s6r.onrender.com/" + req.params.url;
   // res.send(URL)
-  fetch(URL, {headers: {
-    'origin': 'http://localhost:5000',
-    'X-Requested-With': 'XMLHttpRequest'
-  }}).then(data => {
-    // data.header("Access-Control-Allow-Origin", "http://localhost:5500");
-    console.log(data);
-    return data.json();
-  }).then(data => {
-    console.log(data);
-    res.send(data);
+  fetch(URL, {
+    headers: {
+      origin: "http://localhost:5000",
+      "X-Requested-With": "XMLHttpRequest",
+    },
   })
-})
+    .then((data) => {
+      // data.header("Access-Control-Allow-Origin", "http://localhost:5500");
+      console.log(data);
+      return data.json();
+    })
+    .then((data) => {
+      console.log(data);
+      res.send(data);
+    });
+});
 
 app.listen(process.env.PORT || 3000, () => console.log(`Server is running`));
-
